f69ac8cb2ba1979b07c9dad3762ec4d9bceef3aa
SahanSalgado <sahansalgado10@gmail.com>
2024-08-15 13:21:25 +0530

    sha256

--- Blockchain/main.cpp
+++ Blockchain/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <ctime>
+#include <sstream>
+#include <openssl/sha.h>
 
 using namespace std;
 
+string sha256(const string &str);
+
+class Block {
+public:
+    string prevHash;
+    string bHash;
+    string data;
+    time_t timestamp;
+    int nonce;
+
+    Block(string data, string prevHash{
+          this->data = data;
+          this->prevHash = prevHash;
+          this->nonce = 0;
+          this->timestamp = time(nullptr);
+          this->bHash = calcHash();
+    }
+    string calcHash()const{
+        stringstream ss;
+        ss<< prevHash << timestamp << data << nonce;
+        return sha256(ss.str());
+
+    }
+};
 int main()
 {
     cout << "Hello world!" << endl;
